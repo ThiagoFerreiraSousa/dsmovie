b26a690896a401cd2b4b61f038cbadcd3084c9e2
ThiagoFerreiraSousa <thiago.fvs@gmail.com>
2022-01-14 15:30:56 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,26 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
+    const [pageNumber, setPageNumber] = useState(0);
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
+            const data = response.data as MoviePage;
             console.log(response.data);
+            setPageNumber(data.number);            
         });
+    }, []);
     
     return (
         <>
+
             <Pagination />
 
             <div className="container">
